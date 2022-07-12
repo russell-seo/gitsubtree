152830b874116722bf06f62f0ae628eb3661fb64
서상원 <kiaofk@naver.com>
2022-07-12 15:14:51 +0900

    test

---
+++ src/main/java/com/example/githubsubtree/GithubsubtreeApplication.java
@@ -0,0 +1,13 @@
+package com.example.githubsubtree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+class GithubsubtreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubsubtreeApplication.class, args);
+	}
+
+}
